b29c67281fdad2db1e2fe474cd322a9560d33704
sonugit-kumar <136845930+sonugit-kumar@users.noreply.github.com>
2024-05-12 20:27:04 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId =144553
+let accountEmail = "sonu@google.com"
+var accountpassword = "12345"
+accountcity = "patna"
+
+
+//accountEmail =2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountpassword = "2121212121"
+accountCity = "Bangaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountEmail, accountpassword,accountcity]);
