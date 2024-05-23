ec078d800f6d7bbfc35ee4e54320269a42279950
sonugit-kumar <136845930+sonugit-kumar@users.noreply.github.com>
2024-05-23 11:43:13 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treate all js code as newer version.
+
+// alert(3+3) we are using node js, not browser
+
+console.log(3+3); 
+
+console.log("sonu") 
+
+let name = "sonu"
+let age  = 18
+let isloggedin =false
+let state;
+
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undifined => abhi value assing nhi hui hai
+// symbol => uniquness
+// object
+
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
+
