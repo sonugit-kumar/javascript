ba81b612252e4b09eab8a8b4917f4ee28279b835
sonugit-kumar <136845930+sonugit-kumar@users.noreply.github.com>
2024-05-24 18:03:45 +0000

    conversion of datatypes

---
+++ 01_basics/conv_ops.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN // not an number
+// true => 1; false =>0
+
+let isLoggedIn ="sonu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sonu" => true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
